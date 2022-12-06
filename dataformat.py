5edb86c6c43252f4bac6764a22fc1fac1e156a58
Anshul Sharma <anshulsharma1610@gmail.com>
2022-12-05 21:31:08 -0500

    added plots, updated script of data formatting

--- dataformat.py
+++ dataformat.py
@@ -6,6 +6,12 @@ import numpy as np
 import mysql.connector
 from mysql.connector import Error
 import random 
+import os
+import os.path
+import shutil
+d = os.getcwd()
+# os.chdir("..")
+o = [os.path.join(d,o) for o in os.listdir(d) if os.path.isdir(os.path.join(d,o))] # Gets all directories in the folder as a tuple
 
 #database connection
 def create_connection(host_name, user_name, user_password, db):
@@ -19,11 +25,10 @@ def create_connection(host_name, user_name, user_password, db):
         )
         print("Connection to MySQL DB successful")
     except Error as e:
-        print("The error '{e}' occurred")
+        print("The error occurred", e)
     return connection
-connection = create_connection("localhost", "root", "mana@2812", "movie_ticket_reservation")
+connection = create_connection("localhost", "root", "root", "movie_dum")
 cursor = connection.cursor()
-
 
 # # # Load data
 df = pd.read_csv('movies_dataset.csv',encoding='latin-1')
@@ -130,13 +135,16 @@ for movie_id, stars_list in movie_stars_map.items():
     for star in stars_list:
         movie_stars_list.append([movie_id, star])
 
+#remove duplicates  from movie_stars_list
+movie_stars_list = list(map(list, set(map(tuple, movie_stars_list))))
 # # #insert data into movie_stars table in the database
 for i in range(len(movie_stars_list)):
-    print(movie_stars_list[i][0], movie_stars_list[i][1])
+    # print(movie_stars_list[i][0], movie_stars_list[i][1])
     try:
         cursor.execute('''insert into movie_stars (movie_id,star_id)  values (%s, %s)''', (movie_stars_list[i][0], movie_stars_list[i][1]))
-    except:
-        print(movie_stars_list[i][0], movie_stars_list[i][1])
+    except Error as e:
+        print("The error occurred", e)
+        # print(movie_stars_list[i][0], movie_stars_list[i][1])
 connection.commit()
 
 # # #Created List to insert mapping of movie and genre into movie_genre table
@@ -145,50 +153,135 @@ for movie_id, genre_list in movie_genre_map.items():
     for genre in genre_list:
         movie_genre_list.append([movie_id, genre_map[genre.strip()]])
 
+# # #remove duplicates  from movie_genre_list
+movie_genre_list = list(map(list, set(map(tuple, movie_genre_list))))
+
 # # #insert data into movie_genre table in the database
 for i in range(len(movie_genre_list)):
     try:
         cursor.execute('''insert into movie_genre (movie_id, genre_id)  values (%s, %s)''', (movie_genre_list[i][0], movie_genre_list[i][1]))
-    except:
-        print(movie_genre_list[i][0], movie_genre_list[i][1])
+    except Error as e:
+        print("The error occurred", e)
 
 connection.commit()
 
+for item in o:
+    if os.path.exists(item + '\\Dataset Movies - Screens.csv'):
+        file = item + '\\Dataset Movies - Screens.csv'
 
+df4 = pd.read_csv(file,encoding='latin-1')
+
+# insert df4 to screens table in the database
+screens=[]
+for index,row in  df4.iterrows():
+    screens.append([row["Screen ID"],row["Capacity"],row["Theater ID"]])
+
+# for screen in screens:
+#     print(screen[0], screen[1], screen[2])
+#     cursor.execute('''insert into screens (screen_id, capacity, theater_id)  values (%s, %s, %s)''', (screen[0], screen[1], screen[2]))
+# connection.commit()
+
+for item in o:
+    if os.path.exists(item + '\\Dataset Movies - Show Timings.csv'):
+        file = item + '\\Dataset Movies - Show Timings.csv'
+
+df5 = pd.read_csv(file,encoding='latin-1')
+
+# insert df5 to show_timings table in the database
+show_timings=[]
+for index,row in  df5.iterrows():
+    show_timings.append([row["Show Time ID"],row["Start Time"],row["End Time"]])
+
+for show_timing in show_timings:
+    cursor.execute('''insert into show_timings (show_timing_id, start_time, end_time)  values (%s, %s, %s)''', (show_timing[0], show_timing[1], show_timing[2]))
+connection.commit()
+
+for item in o:
+    if os.path.exists(item + '\\Dataset Movies - Shows.csv'):
+        file = item + '\\Dataset Movies - Shows.csv'
     
+df6 = pd.read_csv(file,encoding='latin-1')
 
+# insert df6 to shows table in the database
+shows=[]
+for index,row in  df6.iterrows():
+    shows.append([row["Movie ID"],row["Show Time ID"],row["Screen ID"],row["Ticket Price"],row["Ticket Sold"]])
+
+# for show in shows:
+#     cursor.execute('''insert into screen_shows (movie_id, show_id, screen_id, ticket_price, ticket_sold)  values (%s, %s, %s, %s, %s)''', (show[0], show[1], show[2], show[3], show[4]))
+# connection.commit()
+    
+# for item in o:
+#     if os.path.exists(item + '\\Dataset Movies - Bookings.csv'):
+#         file = item + '\\Dataset Movies - Bookings.csv'
+
+# df7 = pd.read_csv(file,encoding='latin-1')
+
+# # insert df7 to bookings table in the database
+# bookings=[]
+# for index,row in  df7.iterrows():
+#     bookings.append([row["booking_id"],row["user_id"],row["screen_shows_id"],row["date"],row["tickets_booked"]])
+
+# for booking in bookings:
+#     cursor.execute('''insert into bookings (booking_id,user_id, screen_shows_id,created_at, tickets_booked)  values (%s, %s, %s, %s, %s)''', (booking[0], booking[1], booking[2], booking[3], booking[4]))
+# connection.commit()
+
+for item in o:
+    if os.path.exists(item + '\\Dataset Movies - Users.csv'):
+        file = item + '\\Dataset Movies - Users.csv'
+
+df8 = pd.read_csv(file,encoding='latin-1')
+
+# insert df8 to users table in the database
+users=[]
+for index,row in  df8.iterrows():
+    users.append([row["user_id"],row["name"],row["age"], row["gender"], row["email"]])
+
+for user in users:
+    cursor.execute('''insert into user (user_id, name, age, gender, email)  values (%s, %s, %s, %s, %s)''', (user[0], user[1], user[2], user[3], user[4]))
+connection.commit()
+
+for item in o:
+    if os.path.exists(item + '\\Dataset Movies - Transaction.csv'):
+        file = item + '\\Dataset Movies - Transaction.csv'
+
+df9 = pd.read_csv(file,encoding='latin-1')
+
+# insert df9 to transactions table in the database
+transactions=[]
+for index,row in  df9.iterrows():
+    transactions.append([row["id"],row["booking_id"],row["amount"]])
+
+# for transaction in transactions:
+#     cursor.execute('''insert into transaction (id, booking_id, amount)  values (%s, %s, %s)''', (transaction[0], transaction[1], transaction[2]))
+# connection.commit()
 
 # # # # Load data
-# df3 = pd.read_csv('movies_dataset.csv',encoding='latin-1')
+df3 = pd.read_csv('indian-movie-theatres.csv',encoding='latin-1')
 
-# # # # extract unqiue values from city column
-# cities = df3['city'].unique()
+# # # extract unqiue values from city column
+cities = df3['city'].unique()
 
-# # # #create map of cities and unique id
-# city_map = {}
-# for i, city in enumerate(cities):
-#     city_map[city] = i+1
+# # #create map of cities and unique id
+city_map = {}
+for i, city in enumerate(cities):
+    city_map[city] = i+1
 
-# # print(city_map)
+# print(city_map)
 
-# # #create map of values from theatre_name column to no_screens columns and value of city from city_map for city column 
-# # df['city'] = df['city'].map(city_map)
-# # df['theatre_name'] = df['theatre_name']
-# # df['no_screens'] = df['no_screens']
+# # #insert data into city table in the database
+for i in range(len(cities)):
+    cursor.execute('''insert into city (city_id, name)  values (%s, %s)''', (i+1, cities[i]))
+connection.commit()
 
-# # print(df)
-
-# # print(df.head())
-# # # theatres = df['theatre_name'].values
-# # # screens = df['no_screens'].values
-# # # city = [city_map[city] for city in df['city'].values]
-
-# # # theater_map = dict(zip(df['theatre_name'], df['no_screens'], [city_map[city] for city in df['city']]))
-# # # print(theater_map)
-# # city_map={}
-# # # print(cities)
-
-
+theatres = df3[['theatre_name', 'no_screens', 'city']].values
+# print(theatres)
+# #insert data into theatre table in the database
+i=0
+for theatre in theatres:
+    cursor.execute('''insert into theaters (theater_id,name, screens, city_id)  values (%s, %s, %s, %s)''', (i, theatre[0], theatre[1], city_map[theatre[2]]))
+    i+=1
+connection.commit()
 
 
 
